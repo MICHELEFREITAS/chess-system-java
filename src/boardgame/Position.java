2a252551f0b3310904a009f2123e5c415e080101
Michele Freitas <michele-braga@hotmail.com>
2019-08-14 16:12:39 +0100

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,34 @@
+//tabuleiro
+package boardgame;
+
+public class Position {
+	
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getrow() {
+		return row;
+	}
+
+	public void setrow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+}
